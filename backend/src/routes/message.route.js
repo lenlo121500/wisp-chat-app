b4c24b68b8e78588fb70376a1bc5d3a00f4349d8
lenlo121500 <raulc8808@gmail.com>
2025-05-21 17:26:31 +0800

    Fix malformed in the route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -5,6 +5,7 @@ import {
   getUsersForSidebar,
   sendMessage,
 } from "../controllers/message.controller.js";
+import { validateRoutes } from "../validators/route.validator.js";
 
 const messageRouter = express.Router();
 
@@ -13,4 +14,6 @@ messageRouter.use(protectRoute);
 messageRouter.post("/send/:id", sendMessage);
 messageRouter.get("/users", getUsersForSidebar);
 messageRouter.get("/:id", getMessages);
+
+validateRoutes(messageRouter);
 export default messageRouter;
